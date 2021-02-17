d1fa73fda60a12ae1ae30ef73d1a3e0416258acd
izekgolland <izek.go@gmail.com>
2021-02-17 17:29:04 +0200

    add weather

--- js/services/geocoding-service.js
+++ js/services/geocoding-service.js
@@ -8,7 +8,7 @@ function getPosByName(locationName) {
     const API_KEY = 'AIzaSyAGuxwq8wZl4gxL2ERwdbaBPAb6QQl8z94';
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${parseLocation}&key=${API_KEY}`)
         .then(res => {
-            return place = {
+            return {
                 placePos: res.data.results[0].geometry.location,
                 placeName: res.data.results[0].formatted_address
             }
---
+++ js/services/weather-service.js
@@ -0,0 +1,12 @@
+'use strict'
+export const weather = {
+    getWeatherByPos
+}
+
+function getWeatherByPos(pos) {
+    const API_KEY = '5352808304e9484b44aff35cb0fbf76f';
+    return axios.get(`api.openweathermap.org/data/2.5/weather?lat=${pos.lat}&lon=${pos.lng}&units=metric&appid=${API_KEY}`)
+        .then(res => {
+            return res;
+        });
+}
