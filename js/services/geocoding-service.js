3f0c608e4d572b5fd3a155697873a14022b617b5
izekgolland <izek.go@gmail.com>
2021-02-17 17:05:02 +0200

    fixed geocding service

--- js/services/geocoding-service.js
+++ js/services/geocoding-service.js
@@ -4,7 +4,13 @@ export const geoCoding = {
 }
 
 function getPosByName(locationName) {
+    const parseLocation = locationName.replace(' ',',+');
     const API_KEY = 'AIzaSyAGuxwq8wZl4gxL2ERwdbaBPAb6QQl8z94';
-    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${API_KEY}`)
-        .then(res => console.log('Im Here!!!!::::', res.data.results[0].geometry.location));
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${parseLocation}&key=${API_KEY}`)
+        .then(res => {
+            return place = {
+                placePos: res.data.results[0].geometry.location,
+                placeName: res.data.results[0].formatted_address
+            }
+        });
 }
